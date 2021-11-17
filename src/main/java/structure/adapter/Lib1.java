444c10819a774eb8df9ec223d0ac7cb60c8a4d07
Ian Darwin <ian@darwinsys.com>
2021-11-17 08:44:54 -0500

    Make message agree w/ class name

--- src/main/java/structure/adapter/Lib1.java
+++ src/main/java/structure/adapter/Lib1.java
@@ -3,6 +3,6 @@ package structure.adapter;
 public class Lib1 {
 
 	public void doSomething(String name, int number) {
-		System.out.printf("Lib1 got %s, %d\n", name, number);
+		System.out.printf("Lib ONE got %s, %d\n", name, number);
 	}
 }
--- src/main/java/structure/adapter/Lib2.java
+++ src/main/java/structure/adapter/Lib2.java
@@ -3,6 +3,6 @@ package structure.adapter;
 public class Lib2 {
 
 	public void handleData(int number, String name) {
-		System.out.printf("Lib2 got %s, %d\n", name, number);
+		System.out.printf("Lib TWO got %s, %d\n", name, number);
 	}
 }
